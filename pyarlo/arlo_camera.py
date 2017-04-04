f05ef27fdff822497174470e58a5301ffa0392dc
Marcelo Moreira de Mello <tchello.mello@gmail.com>
2017-04-04 02:19:16 -0400

    Added download_snapshot() and snapshot_url  methods.

--- pyarlo/arlo_camera.py
+++ pyarlo/arlo_camera.py
@@ -6,4 +6,16 @@ from pyarlo.arlo_generic import ArloGeneric
 
 class ArloCamera(ArloGeneric):
     """Arlo Camera module implementation."""
-    pass
+
+    @property
+    def snapshot_url(self):
+        """Return snapshot url."""
+        return self._attrs.get('presignedLastImageUrl')
+
+    def download_snapshot(self, filename=None):
+        """Download JPEG snapshot."""
+        if filename is None:
+            ret = self._arlo.session.get(self.snapshot_url)
+            if ret.status_code == 200:
+                return ret.content
+        return False
