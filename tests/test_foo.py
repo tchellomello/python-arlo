9c910e0a2df278a9bf3f1168199b216e50fa3c6d
Marcelo Moreira de Mello <tchello.mello@gmail.com>
2017-04-09 00:58:30 -0400

    Added foo tests for now.

---
+++ tests/test_foo.py
@@ -0,0 +1,10 @@
+"""Foo bar unittest."""
+from unittest import TestCase
+
+
+class TestFooBar(TestCase):
+    """Foo Bar test."""
+
+    def test_blah(self):
+        """Test blah fake."""
+        self.assertTrue(1)
