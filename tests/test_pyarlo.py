47b651236f82ff4552b01610ab9079a459f346bf
Marcelo Moreira de Mello <tchello.mello@gmail.com>
2017-06-30 03:08:23 -0400

    Added unittest for basic PyArlo object testing devices

--- tests/test_pyarlo.py
+++ tests/test_pyarlo.py
@@ -4,7 +4,7 @@ from tests.common import load_fixture
 import requests_mock
 
 from pyarlo.const import (
-    LOGIN_ENDPOINT)
+    DEVICES_ENDPOINT, LOGIN_ENDPOINT)
 
 USERNAME = 'foo'
 PASSWORD = 'bar'
@@ -25,9 +25,49 @@ class TestPyArlo(unittest.TestCase):
 
         mock.post(LOGIN_ENDPOINT,
                   text=load_fixture('pyarlo_authentication.json'))
+
         arlo = PyArlo(USERNAME, PASSWORD, preload=False)
+
         self.assertTrue(arlo.country_code, COUNTRY_CODE)
         self.assertTrue(arlo.authenticated, 1498801924)
         self.assertTrue(arlo.userid, USERID)
         self.assertIsInstance(arlo.ArloMediaLibrary, ArloMediaLibrary)
         self.assertListEqual(arlo.ArloMediaLibrary.videos, [])
+
+    @requests_mock.Mocker()
+    def test_without_preload_devices(self, mock):
+        """Test PyArlo without preloading videos."""
+        from pyarlo import PyArlo
+        from pyarlo.camera import ArloCamera
+        from pyarlo.base_station import ArloBaseStation
+
+        mock.post(LOGIN_ENDPOINT,
+                  text=load_fixture('pyarlo_authentication.json'))
+        mock.get(DEVICES_ENDPOINT,
+                 text=load_fixture('pyarlo_devices.json'))
+
+        arlo = PyArlo(USERNAME, PASSWORD, preload=False)
+
+        arlo_devices = arlo.devices
+        self.assertTrue(arlo_devices.get('base_station', arlo.base_stations))
+        self.assertTrue(arlo_devices.get('cameras', arlo.cameras))
+
+        cameras = arlo.cameras
+        self.assertEquals(len(cameras), 2)
+        for camera in cameras:
+            self.assertIsInstance(camera, ArloCamera)
+            self.assertTrue(camera.device_type, 'camera')
+            self.assertTrue(camera.user_id, USERID)
+
+            if camera.name == 'Front Door':
+                self.assertTrue(camera.device_id, '48B14CAAAAAAA')
+
+            if camera.name == 'Patio':
+                self.assertTrue(camera.model_id, 'VMC3030')
+
+        base = arlo.base_stations[0]
+        self.assertIsInstance(base, ArloBaseStation)
+        self.assertTrue(base.device_type, 'basestation')
+        self.assertTrue(base.user_id, USERID)
+        self.assertTrue(base.hw_version, 'VMB3010r2')
+        self.assertIsNone(base.serial_number)
