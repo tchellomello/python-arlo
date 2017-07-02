aeaa2e7fb0531686482ef45d403e71b826a4985c
chaddotson <chad.dotson@gmail.com>
2017-07-01 22:53:12 -0500

    Fix basestation mode property for custom mode settings. (#28)
    * Fix basestation mode property for custom mode settings.
    Bump version.
    
    * Correct pylint issue... extra else.
    
    * Address comments.
    
    * Address pylint issue. Seems linelength restriction, impairs clarity here.
    
    * Address pylint issue with linelength.

--- pyarlo/base_station.py
+++ pyarlo/base_station.py
@@ -239,8 +239,8 @@ class ArloBaseStation(object):
             modes = mode_event['properties']['modes']
             for mode in modes:
                 if mode['id'] == active_mode:
-                    return mode['type']
-
+                    return mode.get('type') \
+                        if mode.get('type') is not None else mode.get('name')
         return None
 
     @property
