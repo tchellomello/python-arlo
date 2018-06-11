7b206b5b87ecfa3c5d0256667c3d61d5c356dc49
Marcelo Moreira de Mello <tchello.mello@gmail.com>
2018-06-10 23:56:25 -0400

    Added long description to Python package

--- setup.py
+++ setup.py
@@ -8,6 +8,8 @@ setup(
     version='0.1.6',
     description='Python Arlo is a library written in Python 2.7/3x ' +
                 'that exposes the Netgear Arlo cameras as Python objects.',
+    long_description='Python Arlo is a library written in Python 2.7/3x ' +
+                'that exposes the Netgear Arlo cameras as Python objects.',
     author='Marcelo Moreira de Mello',
     author_email='tchello.mello@gmail.com',
     url='https://github.com/tchellomello/python-arlo',
