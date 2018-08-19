c2751ae6ef20f05998c8af5474daf39bb592d55e
Luke Fritz <lukefritz@gmail.com>
2018-08-19 12:02:54 -0500

    Fix linter error

--- tests/common.py
+++ tests/common.py
@@ -65,6 +65,7 @@ def load_audio_playback_status(*args, **kwargs):
     """Load audio playback status."""
     return load_fixture_json("pyarlo_audio_playback.json")
 
+
 def load_extended_properties(*args, **kwargs):
     """Load extended camera properties."""
     return load_fixture_json("pyarlo_extended_properties.json")
